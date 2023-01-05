06977dd9500bc589d4a404def19a4b64d5ea7c87
Антон Дик <antondik@yandex.ru>
2023-01-05 17:36:58 +0300

    Задача 55: Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы. В случае, если это невозможно, программа должна вывести сообщение для пользователя.

---
+++ Zadacha_055/Program.cs
@@ -0,0 +1,81 @@
+﻿// Задача 55: Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы.
+// В случае, если это невозможно, программа должна вывести сообщение для пользователя.
+
+// 4. Метод замены строк на столбцы
+void ChangeArray(int [,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            int temp = arr[i, j];
+            arr[i, j] = arr[j, i];
+            arr[j, i] = temp;
+        }
+    }
+}
+
+// 3. Метод вывода массива в терминал
+void ShowMatrix(int [,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            System.Console.Write("[" + arr[i, j] + "] ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+// 2. Метод заполняет массив рандомно
+void ArrayWithRundomNumbers(int[,] arr, int leftRange, int rightRange)
+{
+    Random rand = new Random();
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i,j] = rand.Next(leftRange, rightRange + 1);
+        }
+    }
+}
+
+// 1. Метод спрашивает пользователя ввести число
+int EnterNumber(string message)
+{
+    System.Console.WriteLine(message);
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+// ================================================================================================
+
+// 1. Пользователь вводит размер массива и границы цифр
+int rows = EnterNumber("Введите кол-во строк: ");
+int columns = EnterNumber("Введите кол-во столбцов: ");
+
+int min = EnterNumber("Укажите левую границу цифр: ");
+int max = EnterNumber("Укажите правую границу цифр: ");
+
+if(rows != columns)
+{
+        System.Console.WriteLine("не получится");
+        
+}
+    
+// 2. Создаётся двумерный массив
+int [,] matrix = new int[rows, columns];
+ArrayWithRundomNumbers(matrix, min, max);
+
+// 3. Вывод массива в терминал 
+ShowMatrix(matrix);
+
+// Вывод пустой строки для визуального разделения массивов
+System.Console.WriteLine();
+
+// 4. Замена строк на столбцы
+ChangeArray(matrix);
+
+// 5. Вывод массива в терминал 
+ShowMatrix(matrix);
